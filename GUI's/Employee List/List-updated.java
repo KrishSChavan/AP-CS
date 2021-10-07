eeb79d0ec29c2696e5a858974745c153d2ac5ac2
Krish <44930634+KrishSChavan@users.noreply.github.com>
2021-10-07 16:31:45 -0400

    Update List-updated.java

--- GUI's/Employee List/List-updated.java
+++ GUI's/Employee List/List-updated.java
@@ -1,24 +1,47 @@
 
 public class List {
-
+	
 	String empName;
 	double q1Sales;
 	double q2Sales;
 	double q3Sales;
 	double q4Sales;
+	double totalSales;
 	
-	List(String name, double q1, double q2, double q3, double q4) {
+	List(String name, double q1, double q2, double q3, double q4, double total) {
 		
 		empName = name;
 		q1Sales = q1;
 		q2Sales = q2;
 		q3Sales = q3;
 		q4Sales = q4;
+		totalSales = q1 + q2 + q3 + q4;
 		
 	}
 	
 }
 
+class Checks {
+	
+	private double highestTotalSalesNum;
+	private int highestTotalSalesIndex;
+	
+	public int getHighest(List[] employees, int count) {
+		
+		for(int i = 0; i < count; i++){
+			if (employees[i].totalSales >= highestTotalSalesNum) {
+				highestTotalSalesNum = employees[i].totalSales;
+				highestTotalSalesIndex =  i;
+			}
+		}
+		
+		System.out.println("Highest Total Sales: " + highestTotalSalesNum + ", by: " + employees[highestTotalSalesIndex].empName);
 
+		
+		return highestTotalSalesIndex;
+		
+	}
+	
+}
 
 // https://www.infoworld.com/article/2076301/learn-how-to-store-data-in-objects.html
