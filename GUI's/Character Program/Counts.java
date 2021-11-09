c90cdd54a9c210d944062d89c51297fd8bddfe02
Krish <44930634+KrishSChavan@users.noreply.github.com>
2021-11-09 08:39:27 -0500

    Update Counts.java

--- GUI's/Character Program/Counts.java
+++ GUI's/Character Program/Counts.java
@@ -1,8 +1,6 @@
 public class Counts {
 	
-	public void enter(String text) {
-		
-		char[] CharArray = new char[text.length()];
+	public void getSpaces(String text) {
 		
 		int spaces = 0;
 		
@@ -11,9 +9,59 @@ public class Counts {
 				spaces++;
 			}
 		}
-		System.out.println("The text you entered has " + (text.length() - spaces) + " ");
-		System.out.println("Has, " + spaces + " spaces");
+		if (spaces != 0) {
+			System.out.println("Characters WITHOUT spaces: " + (text.length() - spaces));
+			System.out.println("Characters WITH spaces: " + text.length());
+			System.out.println("Total Spaces: " + spaces + '\n');
+		} else {
+			System.out.println("Characters: " + text.length());
+			System.out.println("No Spaces Found" + '\n');
+		}
 		
 	}
 		
+	
+	
+	public void getWords(String text) {
+		
+		if (text.trim() != "") {
+			char[] CharArr = new char[text.length()];
+			
+			String[] wordsArr = new String[text.length()];
+			
+			for (int i=0; i<text.length(); i++) {
+				CharArr[i] = text.charAt(i);
+			}
+			
+			String word = "";
+			int wordCount = 0;
+			
+			for (int o=0; o<CharArr.length; o++) {
+				if (Character.isDigit(text.charAt(o)) || Character.isLetter(text.charAt(o))) {
+					word += CharArr[o];
+					System.out.print(CharArr[o]);
+				} else {
+					if (word.trim() != "") {
+						wordsArr[wordCount] = word;
+						wordCount++;
+						word = "";
+						System.out.println();
+					}
+				}
+			}
+			
+			
+			System.out.println('\n' + wordCount + " words found" + '\n');
+			
+			
+			for (int n=0; n<wordCount; n++) {
+				System.out.println("Word " + n + ": " + wordsArr[n]);
+			}
+		}
+		
+	}
+	
+	
+
+	
 }
