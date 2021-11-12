f4668849e32d369cc4c8043006579c2e66e42267
Krish <44930634+KrishSChavan@users.noreply.github.com>
2021-11-12 13:43:00 -0500

    Update Main.java

--- GUI's/Character Program/Main.java
+++ GUI's/Character Program/Main.java
@@ -1,28 +1,40 @@
 import javax.swing.*;
 import BreezySwing.*;
 
-public class Main extends GBFrame {
+public class DecreasingSeq extends GBFrame {
 
 	static JFrame frm;
 	
-	JLabel instructions = addLabel("Type in a sentence...", 1, 1, 1, 1);
+	JLabel instructions = addLabel("Type in a sentence...", 1, 2, 1, 1);
 	
 	JLabel blank = addLabel("", 2, 1, 1, 1);
 	
-	JTextField textField = addTextField("", 3, 1, 1, 1);
+	JTextField textField = addTextField("", 3, 1, 2, 2);
 
     JLabel blank2 = addLabel("", 4, 1, 1, 1);
 
     JButton enterBtn = addButton ("Finished", 5,1,1,1);
-    
+    JButton clearBtn = addButton ("Clear", 5,2,1,1);
     
 
     public void buttonClicked(JButton buttonObj){
     	
     	if (buttonObj == enterBtn) {
     		Counts counts = new Counts();
-    		counts.getSpaces(textField.getText());
-    		counts.getWords(textField.getText() + " ");
+    		String outputString = counts.getSpaces(textField.getText());
+    		int ammntOfWords = counts.storeWords(textField.getText() + " ");
+    		String[] output = new String[ammntOfWords];
+    		
+    		output = counts.printWords(ammntOfWords);
+    		
+    		
+    		
+    		for (int i=0; i<output.length; i++) {
+    			outputString += '\n' + output[i];
+    		}
+    		messageBox(outputString, 300, 200+(output.length*15));
+    	} else if (buttonObj == clearBtn) {
+    		textField.setText("");
     	}
     	
     }
@@ -30,7 +42,7 @@ public class Main extends GBFrame {
     
 
     public static void main(String[] args) {
-        frm = new Main();
+        frm = new DecreasingSeq();
         frm.setTitle ("Decreasing Sequence");
         frm.setSize (400, 250);
         frm.setVisible (true);
