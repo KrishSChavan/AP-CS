12cbbb41c67a37da6b6447d5703e302192a5580a
Krish <44930634+KrishSChavan@users.noreply.github.com>
2021-11-12 13:42:39 -0500

    Update Counts.java

--- GUI's/Character Program/Counts.java
+++ GUI's/Character Program/Counts.java
@@ -1,8 +1,14 @@
 public class Counts {
 	
-	public void getSpaces(String text) {
+	String[] wordsArr;
+	int[] wordCountArr;
+	
+	String[] finalOutput;
+	
+	public String getSpaces(String text) {
 		
 		int spaces = 0;
+		String outputStr = "";
 		
 		for (int k=0; k < text.length(); k++) {
 			if (text.charAt(k) == ' ') {
@@ -13,55 +19,135 @@ public class Counts {
 			System.out.println("Characters WITHOUT spaces: " + (text.length() - spaces));
 			System.out.println("Characters WITH spaces: " + text.length());
 			System.out.println("Total Spaces: " + spaces + '\n');
+			outputStr = "Characters WITHOUT spaces: " + (text.length() - spaces) + '\n' + "Characters WITH spaces: " + text.length() + '\n' + "Total Spaces: " + spaces + '\n';
 		} else {
 			System.out.println("Characters: " + text.length());
 			System.out.println("No Spaces Found" + '\n');
+			outputStr = "Characters: " + text.length() + '\n' + "No Spaces Found" + '\n';
 		}
 		
+		return outputStr;
 	}
 		
 	
 	
-	public void getWords(String text) {
+	public int storeWords(String text) {
+		
+		int wordCount = 0;
 		
 		if (text.trim() != "") {
 			char[] CharArr = new char[text.length()];
 			
-			String[] wordsArr = new String[text.length()];
+			wordsArr = new String[text.length()];
+			wordCountArr = new int[text.length()];
 			
 			for (int i=0; i<text.length(); i++) {
-				CharArr[i] = text.charAt(i);
+				CharArr[i] = Character.toLowerCase(text.charAt(i));
 			}
 			
+			
+			
+			int start = 0;
+			int end = 0;
 			String word = "";
-			int wordCount = 0;
 			
 			for (int o=0; o<CharArr.length; o++) {
-				if (Character.isDigit(text.charAt(o)) || Character.isLetter(text.charAt(o))) {
-					word += CharArr[o];
-					System.out.print(CharArr[o]);
-				} else {
-					if (word.trim() != "") {
+				
+				if (word.length() == 0) {
+					start = o;
+					word = text.substring(start);
+				} else if (CharArr[o] == ' ') {
+					end = o;				
+					word = text.substring(start, end);
+					
+					if (word.length() == 1 && word.trim() != "") {
 						wordsArr[wordCount] = word;
 						wordCount++;
 						word = "";
-						System.out.println();
+					} else if (!Character.isDigit(CharArr[end-1]) && !Character.isLetter(CharArr[end-1])) {
+						
+						if (CharArr[end-1] == '\'') {
+							word = text.substring(start, end);
+						} else {
+							word = text.substring(start, end-1);
+						}
 					}
-				}
+					
+					System.out.println("WORD: " + word);
+					
+					if (wordCount > 0) {
+						
+						boolean wordIsThere = false;
+						
+						for (int i=0; i<wordCount; i++) {
+							if (word.equalsIgnoreCase(wordsArr[i])) {
+//								System.out.println("FOUND IT");
+								wordIsThere = true;
+								wordCountArr[i] = wordCountArr[i] + 1;
+								word = "";
+							} else {
+//								System.out.println("still looking..");
+							}
+						}
+						if (!wordIsThere && word.trim() != "") {
+							wordsArr[wordCount] = word;
+							wordCount++;
+							word = "";
+						}
+						
+					} else {
+						if (word.trim() != "") {
+							wordsArr[wordCount] = word;
+							wordCount++;
+							word = "";
+						}
+					}
+					
+				} 
+				
 			}
 			
 			
-			System.out.println('\n' + wordCount + " words found" + '\n');
+		}
+		
+		return wordCount;
+	}
+
+
+
+	public String[] printWords(int wordCount) {
+		System.out.println(wordCount + " words found:" + '\n');
+		
+		finalOutput = new String[wordCount];
+		
+		for (int n=0; n<wordCount; n++) {
 			
+			int specWordCount = 0;
 			
-			for (int n=0; n<wordCount; n++) {
-				System.out.println("Word " + n + ": " + wordsArr[n]);
+			for (int i=0; i<wordCount; i++) {
+				if (wordsArr[n].equalsIgnoreCase(wordsArr[i]) && wordsArr[n].trim() != "") {
+					wordCountArr[n] = wordCountArr[n] + (specWordCount+1);
+				}
+			}
+			if (wordsArr[n].trim() != "") {
+				
+				String newWord = wordsArr[n].trim();
+				
+				System.out.println(newWord.substring(0,1).toUpperCase() + newWord.substring(1,newWord.length()).toLowerCase() + " - " + wordCountArr[n]);
+				
+				finalOutput[n] = newWord.substring(0,1).toUpperCase() + newWord.substring(1,newWord.length()).toLowerCase() + " - " + wordCountArr[n];
 			}
 		}
 		
+		return finalOutput;
 	}
-	
 	
 
 	
 }
+
+
+
+// My words have many words in them
+
+// hello there there .
