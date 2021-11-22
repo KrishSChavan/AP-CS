345ba8c223d982d3a4c4b016f977f6f848d4934f
Krish <44930634+KrishSChavan@users.noreply.github.com>
2021-11-22 17:48:18 -0500

    Update Main.java

--- GUI's/Arithmetic Program/Main.java
+++ GUI's/Arithmetic Program/Main.java
@@ -1,80 +1,123 @@
 import javax.swing.*;
 import BreezySwing.*;
 
-public class Main extends GBFrame {
-
+public class Main extends GBFrame{
 	
-	// Defining parts of the frame
+	GBPanel gridPanel = addPanel(1,2,1,1);
+	
+	JLabel header = addLabel("Enter Equation:", 1,1,1,1);
+	
+	JTextField textField = addTextField("", 2,1,1,1);
+	
+	JButton calculateBtn = addButton("Finished", 3,1,1,1);
+	
+	boolean allGood = true;
+	
+	public void buttonClicked(JButton Button) {
+		//calculate button
+		if (Button == calculateBtn) {
+            allGood = true;
+            String input = textField.getText();
+            input = input.trim();
+            
+            //error check
+            if(input.charAt(0) != '=') {
+                allGood = false;
+                messageBox(input);
+            }
+            if(allGood == true) {
+                for(int i = 0; i < input.length(); i++) {
+                    if(Character.isLetter(input.charAt(i)) == true) {
+                        allGood = false;
+                        messageBox(input);
+                        break;
+                    }
+                }
+            }
+		//spaces in between numbers
+            char operand = ' ';
+			int operandPos = 0;
+			String checks = "";
+			char mark = ' ';
+			checks = input.substring(1,input.length());
+			checks = checks.trim();
+			if(Character.isDigit(checks.charAt(0)) == false) {
+				mark = checks.charAt(0);
+				checks = checks.substring(1, checks.length());
+			}
+			for(int i = 0; i < checks.length(); i ++) {
+				if(checks.charAt(i) == '-' || checks.charAt(i) == '+' || checks.charAt(i) == '*' || checks.charAt(i) == '/') {
+					operand = checks.charAt(i);
+					operandPos = i;
+					break;
+				}
+			}
+			String first, second;
+			first = checks.substring(0, operandPos);
+			second = checks.substring(operandPos+1, checks.length());
+			first = first.trim();
+			second = second.trim();
+			//part before operand
+			int numberCount = 0;
+			if(first.charAt(0) == '-' || first.charAt(0) == '+' || first.charAt(0) == '*' || first.charAt(0) == '/' || first.charAt(0) == ' ') {
+				first = first.substring(1, first.length());
+				first = first.trim();
+			}
+			for(int i = 0; i < first.length(); i++){
+				if(Character.isDigit(first.charAt(i)) == true) {
+					numberCount++;
+				}
+			}
+			if(numberCount < first.length()) {
+				messageBox("Error");
+				allGood = false;
+			}
+			//part after operand
+			numberCount = 0;
+			char mark2 = ' ';
+			if(second.charAt(0) == '-') {
+				mark2 = second.charAt(0);
+			}
+			if(second.charAt(0) == '-' || second.charAt(0) == '+' || second.charAt(0) == '*' || second.charAt(0) == '/' || first.charAt(0) == ' ') {
+				second = second.substring(1, second.length());
+				second = second.trim();
+			}
+			for(int i = 0; i < second.length(); i++){
+				if(Character.isDigit(second.charAt(i)) == true) {
+					numberCount++;
+				}
+			}
+			if(numberCount < second.length()) {
+				allGood = false;
+				messageBox("Error");
+			}
+			//Signs get added after check
+			if(mark2 == ' ') {
+				input = mark + first + operand + second;
+			}else {
+				input = mark + first + operand + mark2 + second;
+			}
+			input = input.trim();
+			
+			
+			//class
+			if(allGood == true) {
+				input = input.trim();
+				Calculations Calculate = new Calculations();
+				Calculate.setEquation(input);
+				Calculate.findOperand();
+				Calculate.findDigits();
+				messageBox(Calculate.Caculate());
+			}
+		}
+	}
 	
 	
-	static JFrame frm;
-	
-	JLabel instructions = addLabel("TYPE AN EQUATION", 1, 2, 1, 1);
-	
-	JLabel blank = addLabel("", 2, 1, 1, 1);
-	
-	JTextField textField = addTextField("", 3, 1, 4, 1);
-
-    JLabel blank2 = addLabel("", 4, 1, 1, 1);
-
-    JButton enterBtn = addButton ("Finished", 5,1,1,1);
-    JButton clearBtn = addButton ("Clear", 5,3,2,1);
-    
-    String text = "";
-    
-    // Checking for button click
-    
-    public void buttonClicked(JButton buttonObj){
-    	
-    	// On the click of the enter button the program calls the different methods in the Counts class and prints out all the characters and words
-    	
-    	if (buttonObj == enterBtn) {
-    		
-    		text = textField.getText().trim();
-    		
-    		if (textField.getText().isBlank()) {
-    			messageBox("No text entered.");
-    		} else if (text.charAt(0) == '=') {
-    			
-    			// calling calculating methods
-    			
-    			boolean letterFound = false;
-    			
-    			for (int i=0; i<text.length(); i++) {
-    				if (Character.isLetter(text.charAt(i))) {
-    					letterFound = true;
-    					messageBox("Error: Please remove all letters from the input.", 400, 150);
-    					return;
-    				}
-    			}
-    			if (!letterFound) {
-    				Calculations calcs = new Calculations();
-    				char operand = calcs.findOperand(text);
-    				System.out.println("Operand: " + operand);
-    			}
-    			
-    			
-    		} else {    			
-    			messageBox(textField.getText(), textField.getText().length()*6, 250);
-    		}
-    	} else if (buttonObj == clearBtn) {
-    		
-    		// On the click of the clear button the text field is cleared
-    		
-    		textField.setText("");
-    	}
-    	
-    }
-    
-    
-
-    // Creation of the frame
-    
-    public static void main(String[] args) {
-        frm = new Main();
-        frm.setTitle ("Word Finder");
-        frm.setSize (400, 250);
-        frm.setVisible (true);
-    }
-
+	//main
+	public static void main(String[] args) {
+		JFrame frm = new Main();
+		frm.setTitle("Parser Program");
+		frm.setSize(300,120);
+		frm.setVisible(true);
+	}
 }
