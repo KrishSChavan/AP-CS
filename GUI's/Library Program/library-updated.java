66fb1bb6b6cbac21936e143a3be52251e99d7813
Krish <44930634+KrishSChavan@users.noreply.github.com>
2021-12-02 08:24:12 -0500

    Create library-updated.java

---
+++ GUI's/Library Program/library-updated.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+
+public class Library {
+	
+	String bookTitle;
+	String bookAuthor;
+	boolean isBorrowed;
+	String borrower;
+	String dateBorrowed;
+	
+	public Library(String a, String t, boolean borrowed, String personBorrowing, String d) {
+		this.bookAuthor = a;
+		this.bookTitle = t;
+		this.isBorrowed = borrowed;
+		this.borrower = personBorrowing;
+		this.dateBorrowed = d;
+	}
+	
+}
