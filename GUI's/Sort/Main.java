22af5d5b801521f5c63b071c963ee25b09e0cd49
Krish <44930634+KrishSChavan@users.noreply.github.com>
2022-01-28 13:50:43 -0500

    Update Main.java

--- GUI's/Sort/Main.java
+++ GUI's/Sort/Main.java
@@ -7,12 +7,63 @@ public class Main extends GBFrame {
 	static JFrame frm;
 	
 	JLabel ammt = addLabel("0 numbers entered", 1,2,1,1);
-	IntegerField left = addIntegerField (0, 2,2,1,1);
+	JTextField num = addTextField ("", 2,2,1,1);
 	JButton add = addButton ("ADD", 3,2,1,1);
 	JButton mean = addButton ("Mean", 4,1,1,1);
 	JButton median = addButton ("Median", 4,2,1,1);
 	JButton mode = addButton ("Mode", 4,3,1,1);
 	
+	int count = 0;
+	int nums[] = new int[25];
+	boolean thereIsSpace = true;
+	
+	public void buttonClicked(JButton btn) {
+		if (btn == add) {
+			num.setText("");
+			enter(num.getText());
+			if (count == 25) {
+				add.setVisible(false);
+				num.setVisible(false);
+			}
+		} else if (btn == mean) {
+			
+		} else if (btn == median) {
+			
+		} else if (btn == mode) {
+			
+		}
+	}
+	
+	
+	public void enter(String num) {
+		
+		System.out.println(Integer.parseInt(num));
+		
+		try{
+		    int numb = Integer.parseInt(num);
+		    nums[count] = numb;
+			count++;
+			ammt.setText((count) + " numbers entered");
+		}catch (NumberFormatException ex) {
+		    messageBox("Enter an integer value");
+		}
+		
+		
+	}
+	
+//	public double mean() {
+//		for (int i=0; i<count; i++) {
+//			
+//		}
+//	}
+	
+	public void median() {
+		
+	}
+	
+	public void mode() {
+		
+	}
 	
 	public static void main(String[] args) {
 		frm = new Main();
