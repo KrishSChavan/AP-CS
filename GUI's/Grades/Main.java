dbd02312393386422269a1072df8e825b139647c
Krish <44930634+KrishSChavan@users.noreply.github.com>
2022-02-15 08:34:53 -0500

    Update Main.java

--- GUI's/Grades/Main.java
+++ GUI's/Grades/Main.java
@@ -1,5 +1,7 @@
 package sorting;
 import java.awt.Color;
+import java.io.PrintStream;
+
 import javax.swing.*;
 import BreezySwing.*;
 
@@ -7,36 +9,72 @@ public class Main extends GBFrame {
 
 	static JFrame frm;
 	
-	JLabel ammt = addLabel("0 students entered", 1,2,1,1);
+	JButton ammt = addButton("0 students entered", 1,2,1,1);
 	JTextField nameField = addTextField ("", 2,2,1,1);
 	JButton addName = addButton ("ADD", 3,2,1,1);
 	JButton reset = addButton ("Reset", 3,1,1,1);
 	JButton print = addButton ("Print All", 3,3,1,1);
 	
 	int count = 0;
-	int stu[] = new int[15];
+	Student stu[] = new Student[15];
 	boolean thereIsSpace = true;
 	
 	
 	public Main() {
 		addName.setBackground(Color.pink);
 		addName.setForeground(Color.white);
+		
+		ammt.setBackground(Color.GRAY);
+		ammt.setForeground(Color.WHITE);
+		ammt.setBorderPainted(false);
 	}
+	
+	
 	
 	public void buttonClicked(JButton btn) {
 		if (btn == addName && !nameField.getText().isBlank()) {
-			Student k = new Student(nameField.getText(), 0, 0, 0, 0);
-			
-			System.out.println(k.getStu());
+			nameField.requestFocus();
+			add();
+		} else if (btn == print) {
+			printAll();
+		} else if (btn == reset) {
+			reset();
 		}
 	}
 	
 	
+	public void add() {
+		if (count == 15) {
+			addName.setVisible(false);
+			nameField.setVisible(false);
+			return;
+		}
+		
+		stu[count] = new Student(nameField.getText(), 0, 0, 0, 0);
+		nameField.setText("");
+		count++;
+		ammt.setText(count + " students added");
+	}
+	
+	public void printAll() {
+		for (int i=0; i<count; i++) {
+			System.out.println(stu[i].getStu() + " i: " + i);
+		}
+	}
+	
+	public void reset() {
+		count = 0;
+		addName.setVisible(true);
+		nameField.setVisible(true);
+		ammt.setText(count + " students added");
+		nameField.setText("");
+	}
+	
 	public static void main(String[] args) {
 		frm = new Main();
-    frm.setTitle ("Krish's Calculator");
-    frm.setSize (400, 250);
-    frm.setVisible(true);
+        frm.setTitle ("Krish's Calculator");
+        frm.setSize (400, 250);
+        frm.setVisible(true);
 	}
 
 }
