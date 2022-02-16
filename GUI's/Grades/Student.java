b46e02896ddd024c8d93b7b32e0da47491ee9e64
Krish <44930634+KrishSChavan@users.noreply.github.com>
2022-02-16 11:00:05 -0500

    Create Student.java

---
+++ GUI's/Grades/Student.java
@@ -0,0 +1,47 @@
+package sorting;
+
+public class Student {
+
+	private String name = "";
+	private double testAvg = 0;
+	private double quizAvg = 0;
+	private double hwAvg = 0;
+	private double finalAvg = 0;
+	
+	public Student(String n, double tA, double qA, double hA, double fA) {
+		name = n;
+		testAvg = tA;
+		quizAvg = qA;
+		hwAvg = hA;
+		finalAvg = fA;
+	}
+	
+	
+//	Setters
+	
+	public void setTA(double a) {
+		testAvg = a;
+	}
+	
+	public void setQA(double a) {
+		quizAvg = a;
+	}
+
+	public void setHA(double a) {
+		hwAvg = a;
+	}
+
+	public void setFA(double a) {
+		finalAvg = a;
+	}
+	
+	
+	
+//	Getters
+	
+	
+	public String getStu() {
+		return name + " " + hwAvg + " " + quizAvg + " " + testAvg + " " + finalAvg;
+	}
+	
+}
