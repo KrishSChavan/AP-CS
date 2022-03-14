aa456a0a6dbbe5dc448f08a548c489aaf2ab8798
Krish <44930634+KrishSChavan@users.noreply.github.com>
2022-03-14 08:40:03 -0400

    Create Search.java

---
+++ GUI's/Searching/Search.java
@@ -0,0 +1,40 @@
+package sorting;
+
+import java.awt.Color;
+
+import javax.swing.*;
+import BreezySwing.*;
+
+public class Search extends GBDialog {
+
+	JTextField nameFld = addTextField("[Name Here]", 1,1,3,3);
+	JButton ss = addButton("Sequential Search", 2,1,1,1);
+	JButton bs = addButton("Binary Search", 2,3,1,1);
+	
+	JLabel blank = addLabel ("", 3,2,1,1);
+	
+	JButton b1 = addButton ("1", 4,2,1,1);
+	JButton b2 = addButton ("2", 5,2,1,1);
+	JButton b3 = addButton ("3", 6,2,1,1);
+	JButton b4 = addButton ("4", 7,2,1,1);
+	JButton b5 = addButton ("5", 8,2,1,1);
+	
+	JButton left = addButton ("<", 9,1,1,1);
+	JButton right = addButton (">", 9,3,1,1);
+	
+	Person[] per;
+	
+	public Search(JFrame parent, Person[] people){
+		// The next few lines are part of every dialog
+        super (parent);                                 // ** REQUIRED **
+        setTitle ("Search");
+        setDlgCloseIndicator ("Cancel");
+        setSize (400, 650);
+        
+        this.per = people;
+	}
+	
+	
+	
+	
+}
