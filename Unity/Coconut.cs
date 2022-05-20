ad9c827adc846bd41f33f5948f22d632571c7a09
Krish <44930634+KrishSChavan@users.noreply.github.com>
2022-05-19 22:27:21 -0400

    Create Coconut.cs

---
+++ Unity/Coconut.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coconut : MonoBehaviour
+{
+
+    public bool isCol = false;
+    public GameObject[] hearts;
+
+    // Start is called before the first frame update
+    void Start() {
+        hearts = GameObject.FindGameObjectsWithTag("Heart");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) { 
+        if (col.gameObject.tag == "Player" && isCol == false) {
+            isCol = true;
+            Destroy(gameObject);
+            col.gameObject.GetComponent<Move>().lives -= 1;
+            Destroy(hearts[col.gameObject.GetComponent<Move>().lives]);
+            if (col.gameObject.GetComponent<Move>().lives == 0) {
+                Destroy(col.gameObject.GetComponent<Move>());
+                // Destroy(col.gameObject);
+            }
+        } else if (col.gameObject.tag == "Ground" && isCol == false) {
+            isCol = true;
+            Destroy(gameObject);
+        }
+    }
+}
