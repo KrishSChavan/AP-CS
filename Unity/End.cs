e415da58288ba65044d09d30f2dfab1a0f84825e
Krish <44930634+KrishSChavan@users.noreply.github.com>
2022-05-19 22:27:42 -0400

    Create End.cs

---
+++ Unity/End.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+
+    GameObject monke;
+    GameObject baseObj;
+    Rigidbody2D rb;
+    public bool end;
+
+    void Start() {
+        monke = GameObject.Find("Monkey");
+        baseObj = GameObject.Find("Base");
+        // endText = gameObject.GetComponent<Text>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        end = false;
+    }
+
+    void Update() {
+        if (GameObject.Find("Monkey") != null) {
+            if (monke.GetComponent<Move>().lives == 0) {
+                rb.constraints = RigidbodyConstraints2D.None;
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                rb.gravityScale = 1.1f;
+
+                baseObj.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                baseObj.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+                baseObj.GetComponent<Rigidbody2D>().gravityScale = 1.1f;
+
+                Destroy(monke);
+
+                end = true;
+            }
+        } 
+    }
+}
