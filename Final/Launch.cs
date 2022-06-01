7547670875b75f161c2a8792ed71d9a45d25ac9f
Krish <44930634+KrishSChavan@users.noreply.github.com>
2022-06-01 11:02:04 -0400

    Update Launch.cs

--- Final/Launch.cs
+++ Final/Launch.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class Launch : MonoBehaviour {
 
-    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public float thrust;
+    public float speed = 2;
+    public bool goUp = false;
 
     // Start is called before the first frame update
     void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
 
+        if (goUp == true) {
+            rb.velocity = Vector2.up * speed;
+        } else {
+            rb.velocity = Vector2.down * speed;
+        }
+
+        // if (Input.GetKeyDown("space")) {
+        //     print("Space clicked");
+        //     // rb.AddForce(0, thrust, 0, ForceMode.Impulse);
+        //     transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        // }
     }
 
     private void OnCollisionEnter2D(Collision2D col) {
-        if (col.gameObject.tag == "Ball") {
-            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-            transform.position += movement * Time.deltaTime * moveSpeed;
-            // transform.position = new Vector3(11.25f, -1.0f, 0.0f);
+        if (col.gameObject.tag == "highCheck") {
+            goUp = false;
+        } else if (col.gameObject.tag == "Ball") {
+            goUp = true;
         }
     }
 
